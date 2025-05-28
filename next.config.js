37c3486510bd3fbff7d4883eff68be5eb2c03f1c
bhanu prakash chowdam <jcrseduvalley@gmail.com>
2025-05-28 15:33:27 +0530

    deployement erro

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
